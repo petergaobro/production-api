265def63f97eabd7eba3caf65c800662ca44053c
Peter GAO <37321872+petergaobro@users.noreply.github.com>
2025-04-07 13:37:35 +1000

    error handler

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import bookingRouter from "./routes/booking.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // /api/v1/auth/sign-up
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/bookings', bookingRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     // res.render('index');
